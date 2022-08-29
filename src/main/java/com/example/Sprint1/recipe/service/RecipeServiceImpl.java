409b080c9a4f1c3f5bf7de662f8a028efdc5cc5f
JosueCQ <u201415040@upc.edu.pe>
2022-08-28 21:29:26 -0500

    fix: recipeServiceImpl

--- src/main/java/com/example/Sprint1/recipe/service/RecipeServiceImpl.java
+++ src/main/java/com/example/Sprint1/recipe/service/RecipeServiceImpl.java
@@ -1,8 +1,10 @@
-package com.example.Sprint1.service;
+package com.example.Sprint1.recipe.service;
 
-import com.example.Sprint1.domain.entity.Recipe;
-import com.example.Sprint1.domain.persistence.RecipeRepository;
-import com.example.Sprint1.domain.service.RecipeService;
+import com.example.Sprint1.patient.domain.model.Patient;
+import com.example.Sprint1.patient.domain.repository.PatientRepository;
+import com.example.Sprint1.recipe.domain.entity.Recipe;
+import com.example.Sprint1.recipe.domain.persistence.RecipeRepository;
+import com.example.Sprint1.recipe.domain.service.RecipeService;
 import com.example.Sprint1.shared.exception.ResourceNotFoundException;
 import com.example.Sprint1.shared.exception.ResourceValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,32 +27,44 @@ public class RecipeServiceImpl implements RecipeService {
     private RecipeRepository recipeRepository;
 
     @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
     private Validator validator;
 
-    public List<Recipe> getAll() { return recipeRepository.findAll(); }
+    public List<Recipe> getAllRecipes() { return recipeRepository.findAll(); }
 
     @Override
-    public Page<Recipe> getAll(Pageable pageable) {
-        return recipeRepository.findAll(pageable);
-    }
-
-    @Override
-    public Recipe getById(Long recipeId) {
+    public Recipe getRecipeById(Long recipeId) {
         return recipeRepository.findById(recipeId)
                 .orElseThrow(() -> new ResourceNotFoundException(ENTITY, recipeId));
     }
 
     @Override
-    public Recipe create(Recipe request) {
-        Set<ConstraintViolation<Recipe>> violations = validator.validate(request);
-        if (!violations.isEmpty())
-            throw new ResourceValidationException(ENTITY, violations);
+    public List<Recipe> getAllByPatientId(Long patientId) {
+        var exitingPatient = patientRepository.findById(patientId);
 
-        return recipeRepository.save(request);
+        if(exitingPatient.isEmpty())
+            throw new ResourceNotFoundException("Patient", patientId);
+
+        return recipeRepository.findByPatientId(patientId);
     }
 
     @Override
-    public Recipe update(Long recipeId, Recipe request) {
+    public Recipe createRecipe(Long patientId, Recipe request) {
+        Set<ConstraintViolation<Recipe>> violations = validator.validate(request);
+
+        if (!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+        return patientRepository.findById(patientId).map(patient -> {
+            request.setPatient(patient);
+            return recipeRepository.save(request);
+        }).orElseThrow(() -> new ResourceNotFoundException("Patient", patientId));
+    }
+
+    @Override
+    public Recipe updateRecipe(Long recipeId, Recipe request) {
         Set<ConstraintViolation<Recipe>> violations = validator.validate(request);
         if (!violations.isEmpty())
             throw new ResourceValidationException(ENTITY, violations);
@@ -60,12 +74,13 @@ public class RecipeServiceImpl implements RecipeService {
                         recipe.withProduct(request.getProduct())
                                 .withWight(request.getWight())
                                 .withCant(request.getCant())
-                                .withEachHour(request.getEachHour()))
+                                .withEachHour(request.getEachHour())
+                                .withCantTomas(request.getCantTomas()))
         ).orElseThrow(() -> new ResourceNotFoundException(ENTITY, recipeId));
     }
 
     @Override
-    public ResponseEntity<?> delete(Long recipeId) {
+    public ResponseEntity<?> deleteRecipe(Long recipeId) {
         return recipeRepository.findById(recipeId).map(recipe -> {
             recipeRepository.delete(recipe);
             return ResponseEntity.ok().build();
